e40e9564d740bae342d210e6361bb73b197fa65e
Udemezue John <johnzeus14@gmail.com>
2024-12-20 19:13:26 +0100

    Updated jobs with a new component

--- components/Jobs.vue
+++ components/Jobs.vue
@@ -1,27 +1,29 @@
 <template>
   <div class = "bg-gray-100 flex items-center justify-center p-12 space-y-5 flex-col">
-    <h1 class="text-xl">Job Board</h1>
-    <div v-if="jobs.length">
-      <div v-for="job in jobs" :key="job.id" class="bg-white space-y-12 w-full p-6">
+    <h1 class="text-xl">Latest Jobs</h1>
+    
+  
+    <ul role="list" class="divide-y divide-gray-100 bg-white space-y-12 w-2/4 p-6" v-for="job in jobs" :key="job.id">
 
-
-        <div class="flex">
-
-        <img :src="job.company_logo" alt="Company Logo" />
-        <h2>{{ job.title }}</h2> 
-        <p>{{ job.company_name }}</p>
-        <p>{{ job.description }}</p>
-        <p>{{ job.salary }}</p>
-        </div>
-       
-        <div>
-          <span v-for="tag in job.tag" :key="tag">{{ tag }}</span>
-        </div>
+  <li class="flex justify-between gap-x-6 py-5">
+    <div class="flex min-w-0 gap-x-4">
+      <img class="size-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+      <div class="min-w-0 flex-auto">
+        <p class="text-sm/6 font-semibold text-gray-900">{{ job.company_name }}</p>
+        <p class="mt-1 truncate text-xs/5 text-gray-500">{{job.title}}</p>
       </div>
+
     </div>
-    <div v-else>
-      <p>No jobs available</p>
+    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+      <p class="text-sm/6 text-gray-900">{{job.tag}}</p>
+      <p class="mt-1 text-xs/5 text-gray-500">Posted <time datetime="2023-01-23T13:23Z">3h ago</time></p>
     </div>
+  </li>
+
+</ul>
+
+
+
   </div>
 </template>
 
