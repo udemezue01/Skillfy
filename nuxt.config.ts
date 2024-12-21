3bae908a050dbb867a75a121a5cdb2395d9acc03
Udemezue John <johnzeus14@gmail.com>
2024-12-21 07:45:05 +0100

    updated dependecies for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
+  // ssr: false, // Cloudflare Pages is best suited for static site generation
+  // target: 'static',
+  // app: {
+  //   baseURL: '/', // Adjust if deploying to a subdirectory
+  // },
+  // generate: {
+  //   fallback: '404.html', // Ensure fallback for static hosting
+  // },
+
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
