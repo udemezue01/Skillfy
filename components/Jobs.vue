ed2459d3cb4f01c55679bb01c1076286535e3f55
Udemezue John <johnzeus14@gmail.com>
2025-01-10 13:02:22 +0100

    customized job Listing

--- components/Jobs.vue
+++ components/Jobs.vue
@@ -3,14 +3,17 @@
     <h1 class="text-xl">Latest Jobs</h1>
     
   
-    <ul role="list" class="divide-y divide-gray-100 bg-white space-y-12 w-full lg:w-2/4 p-6" v-for="job in jobs" :key="job.id">
+    <ul role="list" class="divide-y divide-gray-100 bg-white space-y-12 w-full lg:w-4/6 p-6" v-for="job in jobs" :key="job.id">
 
-  <li class="flex justify-between gap-x-6 py-5">
-    <div class="flex min-w-0 gap-x-4">
+  <li class="flex justify-between gap-x-6 py-5 shadown-sm rounded-sm ">
+    <div class="flex min-w-0 gap-x-4 ">
       <img class="size-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
       <div class="min-w-0 flex-auto">
-        <p class="text-sm/6 font-semibold text-gray-900">{{ job.company_name }}</p>
-        <p class="mt-1 truncate text-xs/5 text-gray-500">{{job.title}}</p>
+        <p class="text-sm/6 font-semibold text-gray-800">{{ job.company_name }}</p>
+        <p class="mt-1 fonttruncate text-md text-gray-900">{{job.title}}</p>
+
+        <p class="mt-1 truncate text-sm text-gray-800">{{job.tag}}</p>
+
       </div>
 
     </div>
